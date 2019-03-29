c4c4709849b6f85a884156d579297b285b7226ed
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-03-30 02:00:51 +0700

    updated post.lua

--- post.lua
+++ post.lua
@@ -1,6 +1,3 @@
--- example HTTP POST script which demonstrates setting the
--- HTTP method, body, and adding a header
-
 wrk.method = "POST"
 wrk.body = "{\"query\": \"query {users{id name}}\"}"
 wrk.headers["Content-Type"] = "application/json"
